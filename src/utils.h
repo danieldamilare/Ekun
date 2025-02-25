06eb221387b24aa0b91b0d9101c3e9019a29b95d
Daniel Joseph <danielomololu2002@gmail.com>
2025-02-25 13:41:59 +0100

    Remove unnecessary files

--- src/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef __UTILS__H
-#define __UTILS__H
-#include <assert.h>
-#include <stddef.h>
-#include <stdbool.h>
-#ifdef EMSCRIPTEN
-void custom_printf(const char * format, ...);
-#endif
-#endif
